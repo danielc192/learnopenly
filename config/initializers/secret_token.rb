acae4bc3059f1bda304492abfe3fbcfa8d166749
Daniel Cohen <danielc192@gmail.com>
2011-10-19 18:53:39 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Learnopenly::Application.config.secret_token = '716a9d1117cf10f2a4a1b42aa0e53fdbda0b8028295e048aed1196f74374fd20add7fbe345711d8ae14823113b5909f1e91dbcaad23ff9a5fe624147b100e624'
